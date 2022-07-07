9e601d0abe0c046eb62b87b0e0ecaef6baba3886
Eder Soares <edersoares@me.com>
2022-07-06 23:25:39 -0300

    Registra as rotas legadas do módulo

--- src/Providers/LibraryServiceProvider.php
+++ src/Providers/LibraryServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace iEducar\Packages\Library\Providers;
 
+use App\Http\Controllers\LegacyController;
 use Illuminate\Support\ServiceProvider;
 
 class LibraryServiceProvider extends ServiceProvider
@@ -11,5 +12,113 @@ class LibraryServiceProvider extends ServiceProvider
         if ($this->app->runningInConsole()) {
             $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
         }
+
+        LegacyController::resolver(function ($uri) {
+            if (in_array($uri, LibraryServiceProvider::intranet())) {
+                return __DIR__ . '/../../ieducar/' . $uri;
+            }
+
+            return null;
+        });
+    }
+
+    public static function intranet(): array
+    {
+        return [
+            'intranet/educar_acervo_assunto_cad.php',
+            'intranet/educar_acervo_assunto_det.php',
+            'intranet/educar_acervo_assunto_lst.php',
+            'intranet/educar_acervo_autor_cad.php',
+            'intranet/educar_acervo_autor_cad_pop.php',
+            'intranet/educar_acervo_autor_det.php',
+            'intranet/educar_acervo_autor_lst.php',
+            'intranet/educar_acervo_cad.php',
+            'intranet/educar_acervo_colecao_cad.php',
+            'intranet/educar_acervo_colecao_cad_pop.php',
+            'intranet/educar_acervo_colecao_det.php',
+            'intranet/educar_acervo_colecao_lst.php',
+            'intranet/educar_acervo_colecao_xml.php',
+            'intranet/educar_acervo_det.php',
+            'intranet/educar_acervo_editora_cad.php',
+            'intranet/educar_acervo_editora_cad_pop.php',
+            'intranet/educar_acervo_editora_det.php',
+            'intranet/educar_acervo_editora_lst.php',
+            'intranet/educar_acervo_editora_xml.php',
+            'intranet/educar_acervo_idioma_cad.php',
+            'intranet/educar_acervo_idioma_cad_pop.php',
+            'intranet/educar_acervo_idioma_det.php',
+            'intranet/educar_acervo_idioma_lst.php',
+            'intranet/educar_acervo_lst.php',
+            'intranet/educar_biblioteca_cad.php',
+            'intranet/educar_biblioteca_dados_cad.php',
+            'intranet/educar_biblioteca_dados_cad_.php',
+            'intranet/educar_biblioteca_dados_det.php',
+            'intranet/educar_biblioteca_dados_lst.php',
+            'intranet/educar_biblioteca_det.php',
+            'intranet/educar_biblioteca_index.php',
+            'intranet/educar_biblioteca_lst.php',
+            'intranet/educar_biblioteca_tipo_cad.php',
+            'intranet/educar_biblioteca_xml.php',
+            'intranet/educar_categoria_cad.php',
+            'intranet/educar_categoria_lst.php',
+            'intranet/educar_categoria_obra_det.php',
+            'intranet/educar_cliente_cad.php',
+            'intranet/educar_cliente_det.php',
+            'intranet/educar_cliente_lst.php',
+            'intranet/educar_cliente_tipo_cad.php',
+            'intranet/educar_cliente_tipo_det.php',
+            'intranet/educar_cliente_tipo_lst.php',
+            'intranet/educar_cliente_tipo_xml.php',
+            'intranet/educar_colecao_xml.php',
+            'intranet/educar_define_status_cliente_cad.php',
+            'intranet/educar_definir_cliente_tipo_cad.php',
+            'intranet/educar_definir_cliente_tipo_det.php',
+            'intranet/educar_definir_cliente_tipo_lst.php',
+            'intranet/educar_editora_xml.php',
+            'intranet/educar_exemplar_baixa.php',
+            'intranet/educar_exemplar_cad.php',
+            'intranet/educar_exemplar_det.php',
+            'intranet/educar_exemplar_devolucao_cad.php',
+            'intranet/educar_exemplar_devolucao_det.php',
+            'intranet/educar_exemplar_devolucao_lst.php',
+            'intranet/educar_exemplar_emprestimo_cad.php',
+            'intranet/educar_exemplar_emprestimo_det.php',
+            'intranet/educar_exemplar_emprestimo_login_cad.php',
+            'intranet/educar_exemplar_emprestimo_lst.php',
+            'intranet/educar_exemplar_lst.php',
+            'intranet/educar_exemplar_renovacao_cad.php',
+            'intranet/educar_exemplar_tipo_cad.php',
+            'intranet/educar_exemplar_tipo_det.php',
+            'intranet/educar_exemplar_tipo_lst.php',
+            'intranet/educar_exemplar_tipo_xml.php',
+            'intranet/educar_fonte_cad.php',
+            'intranet/educar_fonte_det.php',
+            'intranet/educar_fonte_lst.php',
+            'intranet/educar_fonte_xml.php',
+            'intranet/educar_funcao_det.php',
+            'intranet/educar_idioma_xml.php',
+            'intranet/educar_motivo_baixa_cad.php',
+            'intranet/educar_motivo_baixa_det.php',
+            'intranet/educar_motivo_baixa_lst.php',
+            'intranet/educar_motivo_suspensao_cad.php',
+            'intranet/educar_motivo_suspensao_det.php',
+            'intranet/educar_motivo_suspensao_lst.php',
+            'intranet/educar_pagamento_multa_cad.php',
+            'intranet/educar_pagamento_multa_det.php',
+            'intranet/educar_pagamento_multa_lst.php',
+            'intranet/educar_pesquisa_acervo_lst.php',
+            'intranet/educar_pesquisa_cliente_lst.php',
+            'intranet/educar_pesquisa_obra_lst.php',
+            'intranet/educar_reservas_cad.php',
+            'intranet/educar_reservas_det.php',
+            'intranet/educar_reservas_login_cad.php',
+            'intranet/educar_reservas_lst.php',
+            'intranet/educar_situacao_cad.php',
+            'intranet/educar_situacao_det.php',
+            'intranet/educar_situacao_lst.php',
+            'intranet/educar_situacao_xml.php',
+            'intranet/educar_tombo_automatico.ajax.php',
+            'intranet/educar_usuario_cad.php',
+        ];
     }
 }
